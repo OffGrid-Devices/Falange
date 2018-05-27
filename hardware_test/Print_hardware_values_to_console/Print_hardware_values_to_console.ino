c745fb6e119055aeed518cec76eb733924f0a9fb
p1nh0 <p1nh0.c0d1ng@gmail.com>
2018-05-27 01:41:47 +0100

    added hardware test

---
+++ hardware_test/Print_hardware_values_to_console/Print_hardware_values_to_console.ino
@@ -0,0 +1,50 @@
+// hardware pins on arduino
+#define LED 3
+#define SWITCH1 4
+#define SWITCH2 7
+#define KNOB1 2
+#define KNOB2 3
+#define KNOB3 4
+#define KNOB4 5
+#define KNOB5 6
+#define KNOB6 7
+
+int counter = 0; // counter to blink led
+
+void setup() {
+  // start serial communication (to print to console)
+  Serial.begin(9600); 
+  // setup pins as input or output
+  pinMode(LED, OUTPUT); 
+  pinMode(SWITCH1, INPUT);
+  pinMode(SWITCH2, INPUT);
+}
+
+void loop() {
+  Serial.print(digitalRead(SWITCH1));
+  Serial.print(" ");
+  Serial.print(digitalRead(SWITCH2));
+  Serial.print(" ");
+  Serial.print(analogRead(KNOB1));
+  Serial.print(" ");
+  Serial.print(analogRead(KNOB2));
+  Serial.print(" ");
+  Serial.print(analogRead(KNOB3));
+  Serial.print(" ");
+  Serial.print(analogRead(KNOB4));
+  Serial.print(" ");
+  Serial.print(analogRead(KNOB5));
+  Serial.print(" ");
+  Serial.println(analogRead(KNOB6));
+  
+  blinkLed();
+  delay(10);
+}
+
+void blinkLed(){
+  counter+=10;
+  if(counter > 999) counter = 0; 
+
+  if(counter < 550) digitalWrite(LED, HIGH);
+  else digitalWrite(LED, LOW); 
+}
